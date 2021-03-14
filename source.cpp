918b4ebf5fcfdf690abd08c5de480624f15257d9
totallynotusman <80638754+totallynotusman@users.noreply.github.com>
2021-03-14 21:42:45 +0500

    Added some information

--- source.cpp
+++ source.cpp
@@ -7,5 +7,6 @@ int main()
 
 	cout << "03-134182-088 Usman Hamid 6th Semester" << endl;
 	cout<<" 03-134182-037 Farhan Ashraf 6th Semester"<< endl;
+	cout<<" 03-134182-0xx Some name  some Semester"<< endl;
 	return 0;
 }
