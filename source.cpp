1ca6ed16a64b7ad36fc0cc227ddb66b59f530d06
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-03-14 22:33:41 +0500

    added comments
    to elaborate it more and make it more beginners freindly

--- source.cpp
+++ source.cpp
@@ -5,8 +5,74 @@ using namespace std;
 int main()
 {
 
+	
+	cout<<"Student who contributed in this repo on 15th march 2021"<<endl;
 	cout << "03-134182-088 Usman Hamid 6th Semester" << endl;
 	cout<<" 03-134182-037 Farhan Ashraf 6th Semester"<< endl;
+	
+	/*
+	Copy this line and paste it somewhere down in the code and replace details with yours
+	
 	cout<<" 03-134182-0xx Some name  some Semester"<< endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	return 0;
 }
