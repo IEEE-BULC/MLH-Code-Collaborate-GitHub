ad5448e45f3a3be7b2d4f2ff365ebb995c618aa9
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-03-19 15:05:29 -0700

    code allignment

--- source.cpp
+++ source.cpp
@@ -8,94 +8,23 @@ int main()
 	cout<<" 03-134182-037 Farhan Ashraf 6th Semester"<< endl;
 	cout<<"03-134211-050 Faheem Haider 1st Semester"<<endl;
 	cout<<" 03-134841-025 Someones name some semester" <<endl;
-	
-
 	cout << "03-135211-023 Zain Ahmed 1st Semester\n";
 	cout<< "03-134182-041 Sameer Tahir 6th semester"<< endl;
 	cout<< "03-134191-030 Jahanzaib Nazir 5th Semester"<<endl;
-  cout<<" 03-134201-025 Armaghan bin zahid 3th Semester"<< endl;
+	cout<<" 03-134201-025 Armaghan bin zahid 3th Semester"<< endl;
 	cout<<" 090-waqas-2"<< endl;
-  cout<<" 03-135211_036 Haseebullah 1st Semester"<< endl;
-
-
+  	cout<<" 03-135211_036 Haseebullah 1st Semester"<< endl;
 	/*
 	copy this line and paste it  
 	                              
 	cout<<" enrollment Name semester "<< endl;
 	
 	*/
-
 	cout<<" 03-135211_036 Haseebullah 1st Semester"<< endl;
-  cout <<" 03-134202-060 Muhammad Saad 2nd Semester"<<endl;
+  	cout <<" 03-134202-060 Muhammad Saad 2nd Semester"<<endl;
 	cout<<"03-134202-090 Waqas anwar 2nd Semester"<<endl;
 	cout<<"some information"<<endl;
 	cout << "03-134202-035 Abdullah Javed 2nd Semester" << endl;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	cout<<"03-134192-053 Daud Suleman 4th Semester"<<endl;
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
+	cout<<"03-134192-053 Daud Suleman 4th Semester"<<endl;	
 	return 0;
 }
